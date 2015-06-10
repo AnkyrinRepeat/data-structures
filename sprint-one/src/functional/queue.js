199f607910f845a6cb4d22b9b1436ec886c82145
MKS-students <mks.students@hackreactor.com>
2015-06-10 12:33:29 -0500

    functional Queue started

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,32 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    var lastKey = someInstance.size();
+    var nextKey = 1+lastKey;
+    someInstance[nextKey] = value;
+
   };
 
   someInstance.dequeue = function(){
+    var temp = this['1'];
+    delete this['1'];
+    for (var key in this) {
+      if (typeof this[key] !== 'function') {
+        this[key-1] = this[key];
+      }
+    }
+    delete this[this.size()];
   };
 
   someInstance.size = function(){
+    var counter = 0;
+    for (var key in this) {
+      if (typeof this[key] !== 'function') {
+        counter += 1;
+        console.log(key);
+      }
+    }
+    return counter;
   };
 
   return someInstance;
